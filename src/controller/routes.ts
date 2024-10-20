d43c959cd8e13f66d0e5a17ad2542e9295f3e3a7
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-10-20 19:17:36 +0200

    fix: fix status code for error

--- src/controller/routes.ts
+++ src/controller/routes.ts
@@ -14,7 +14,7 @@ function handleNotFound(req: IncomingMessage, res: ServerResponse) {
 
 function handleError(req: IncomingMessage, res: ServerResponse, err: any) {
     if (err instanceof ValidationError) {
-        res.writeHead(404, { "Content-Type": "application/json" });
+        res.writeHead(400, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ error: err.message, errors: err.errors }));
         return;
     }
